8cb7e3c05ddab458ffe9b119c2d527d34530b99c
Nithin G <122164879+nithinganesh1@users.noreply.github.com>
2023-09-19 19:44:08 +0530

    Update input_output.c

--- code-growth/input_output.c
+++ code-growth/input_output.c
@@ -1,1 +1,11 @@
-#include <stdio.h> #include <stdlib.h>int main(void) { 	int a,b,sum; //initialising data type and variable	printf("enter 2 number :\n");  // printing or Display	scanf("%d %d",&a, &b);  // input 	sum = a+b; printf("sum of %d and %d = %d",a,b,sum);	return 0; }
+#include <stdio.h> 
+#include <stdlib.h>
+
+int main(void) { 
+	int a,b,sum; //initialising data type and variable
+	printf("enter 2 number :\n");  // printing or Display
+	scanf("%d %d",&a, &b);  // input 
+	sum = a+b; printf("sum of %d and %d = %d",a,b,sum);
+	return 0; 
+}
+
