fce8ef7d10d274e0aa3aeecabc7e322c7579cf68
Nithin G <122164879+nithinganesh1@users.noreply.github.com>
2023-09-22 21:44:53 +0530

    Add files via upload

---
+++ code-growth/code16_string.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void) {
+	//trying string
+	char name[10];
+	printf("Enter Your Name : ");
+	scanf("%s",name);
+	printf("You Entered %s",name);
+	return 0;
+}
